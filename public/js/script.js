6fdc16dc389b281827d50291facafd2b7a87fb45
Lisu35 <baodihuynh@gmail.com>
2020-12-26 19:51:20 +0700

    finish chat room features

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,8 @@ const myPeer = new Peer(undefined, {
   port: 3001,
 });
 
+faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+
 socket.on("user-disconnected", (userID) => {
   console.log("VO DAY ROI NE");
   $.ajax({
@@ -53,11 +55,41 @@ myPeer.on("open", (userID) => {
 });
 
 const AddVideoStream = (video, stream) => {
+  video.height = 560;
+  video.width = 720;
+  const displaySize = {
+    width: video.width,
+    height: video.height,
+  };
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
     video.play();
   });
-  videoGrid.append(video);
+  video.addEventListener("playing", () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    console.log(canvas);
+    faceapi.matchDimensions(canvas, displaySize);
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(
+        video,
+        new faceapi.TinyFaceDetectorOptions()
+      );
+      // console.log(detections[0]);
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+      if (detections[0] != undefined) {
+        const box = detections[0].box;
+        const x = box.x;
+        const y = box.y;
+        const width = box.width;
+        const height = box.height;
+        const img = new Image();
+        img.src = "/images/spider-man.png";
+        canvas.getContext("2d").drawImage(img, x, y, width, height);
+      }
+    }, 100);
+    videoGrid.append(video);
+    document.body.append(canvas);
+  });
 };
 
 const ConnectToNewUser = (newUserID, stream) => {
