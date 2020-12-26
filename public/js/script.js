d408cd709185f55f1579bdd6c066f392f58bae4f
Lisu35 <baodihuynh@gmail.com>
2020-12-26 22:15:11 +0700

    finish chat room features

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,40 @@
 const firstVideo = document.getElementById("firstVideo");
 const secondVideo = document.getElementById("secondVideo");
-const displaySizeFirst = {
-  width: firstVideo.width,
-  height: firstVideo.height,
+const isModeFirst = true;
+const isModeSecond = false;
+const videoWrapperFirst = document.getElementById("videoWrapperFirst");
+const videoWrapperSecond = document.getElementById("videoWrapperSecond");
+
+const startVideo = () => {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: true,
+    })
+    .then((stream) => {
+      AddVideoStream(firstVideo, stream, isModeFirst, videoWrapperFirst);
+
+      myPeer.on("call", (call) => {
+        call.answer(stream);
+        call.on("stream", (userVideoStream) => {
+          AddVideoStream(
+            secondVideo,
+            userVideoStream,
+            isModeSecond,
+            videoWrapperSecond
+          );
+        });
+      });
+
+      socket.on("user-connected", (newUserID) => {
+        ConnectToNewUser(newUserID, stream);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
-const displaySizeSecond = {
-  width: secondVideo.width,
-  height: secondVideo.height,
-};
+
 const socket = io("/");
 
 const myPeer = new Peer(undefined, {
@@ -15,7 +42,9 @@ const myPeer = new Peer(undefined, {
   port: 3001,
 });
 
-// faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri("/models")]).then(
+  startVideo
+);
 
 socket.on("user-disconnected", (userID) => {
   $.ajax({
@@ -32,67 +61,60 @@ socket.on("user-disconnected", (userID) => {
   });
 });
 
-navigator.mediaDevices
-  .getUserMedia({
-    video: true,
-    audio: true,
-  })
-  .then((stream) => {
-    AddVideoStream(firstVideo, stream);
-
-    myPeer.on("call", (call) => {
-      call.answer(stream);
-      call.on("stream", (userVideoStream) => {
-        AddVideoStream(secondVideo, userVideoStream);
-      });
-    });
-
-    socket.on("user-connected", (newUserID) => {
-      ConnectToNewUser(newUserID, stream);
-    });
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
 myPeer.on("open", (userID) => {
   socket.emit("join-room", ROOM_ID, userID);
 });
 
-const AddVideoStream = (video, stream) => {
+const AddVideoStream = (video, stream, isMode, wrapper) => {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
     video.play();
   });
-  // video.addEventListener("playing", () => {
-  //   const canvas = faceapi.createCanvasFromMedia(video);
-  //   faceapi.matchDimensions(canvas, displaySize);
-  //   setInterval(async () => {
-  //     const detections = await faceapi.detectAllFaces(
-  //       video,
-  //       new faceapi.TinyFaceDetectorOptions()
-  //     );
-  //     // console.log(detections[0]);
-  //     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-  //     if (detections[0] != undefined) {
-  //       const box = detections[0].box;
-  //       const x = box.x;
-  //       const y = box.y;
-  //       const width = box.width;
-  //       const height = box.height;
-  //       const img = new Image();
-  //       img.src = "/images/spider-man.png";
-  //       canvas.getContext("2d").drawImage(img, x, y, width, height);
-  //     }
-  //   }, 100);
-  //   document.body.append(canvas);
-  // });
+  if (isMode) {
+    const displaySize = {
+      width: video.width,
+      height: video.height,
+    };
+    video.addEventListener("playing", () => {
+      const canvas = faceapi.createCanvasFromMedia(video);
+      canvas.width = displaySize.width;
+      wrapper.append(canvas);
+      // document.body.append(canvas);
+      faceapi.matchDimensions(canvas, displaySize);
+      setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(
+          video,
+          new faceapi.TinyFaceDetectorOptions()
+        );
+        if (detections[0] != undefined) {
+          const box = detections[0].box;
+          // console.log(box);
+          const x = box.x;
+          const y = box.y;
+          const width = box.width;
+          const height = box.height;
+          const img = new Image();
+          img.src = "/images/spider-man.png";
+          canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+          canvas
+            .getContext("2d")
+            .drawImage(img, x + 20, y - 20, width + 40, height + 40);
+        }
+      }, 100);
+    });
+  }
 };
 
 const ConnectToNewUser = (newUserID, stream) => {
   const call = myPeer.call(newUserID, stream);
   call.on("stream", (userVideoStream) => {
-    AddVideoStream(secondVideo, userVideoStream);
+    AddVideoStream(
+      secondVideo,
+      userVideoStream,
+      isModeSecond,
+      videoWrapperSecond,
+      secondVideoCanvas
+    );
   });
   call.on("close", () => {
     video.remove();
